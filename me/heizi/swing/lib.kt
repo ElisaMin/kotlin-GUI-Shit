b331e33a84e36458321038fe9305ea4d390a52ee
Heizi <xm.zhongjiaxin@gmail.com>
2020-05-08 20:03:33 +0800

    添加注解 整理文件结构

--- me/heizi/swing/lib.kt
+++ me/heizi/swing/lib.kt
@@ -1,2 +1,70 @@
 package me.heizi.swing
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import me.heizi.utills.log
+import me.heizi.utills.platformTool
+import java.io.IOException
+import javax.swing.JDialog
+
+inline infix fun Boolean.True(block: (Boolean)->Unit):Boolean = this.also { if (it) block(this) }
+inline infix fun Boolean.False(block:(Boolean)->Unit):Boolean = this.also { if (!it) block(this) }
+fun String.toHtml(): String = """<html>
+    <body><div>${this.replace("\n","<br />")}</div>
+</body></html>""".trimIndent().println()
+
+fun <T> T.println() : T {
+    kotlin.io.println(toString())
+    return this
+}
+
+fun waitForDeviceFastboot(dosth:()->Unit){
+    var dialog: JDialog? = null
+    GlobalScope.launch {
+        launch(Dispatchers.IO){
+            dialog = TextDialog(
+                title = "等待设备",
+                show = true
+            ){
+                "等待设备中，当检测到单个fastboot设备后，软件会自动跳转到下一个步骤。请将手机重启到fastboot再启动本软件，并且检查驱动是否成功安装到你的电脑上。" +
+                        "<br />相关视频：如何给电脑安装fastboot驱动 <br /> <a href='https://www.bilibili.com/video/BV1n64y1u7LE/'>https://www.bilibili.com/video/BV1n64y1u7LE/</a>"
+            }
+        }
+
+        launch(Dispatchers.Default) {
+            try {
+                loop@ while (true) {
+                    if (dialog != null){if (!dialog!!.isVisible) {
+                        println("窗口关闭")
+                        break@loop
+                    }}
+                    val (b, s) = platformTool fastboot "devices"
+                    if (b) {
+                        when (s!!.lines().size) {
+                            0 -> throw IOException("未知错误")
+                            1 -> log("未检测到设备")
+                            2 -> {
+                                dialog?.isVisible = false
+                                dosth()
+                                break@loop
+                            }
+                            else -> println("多台设备，请拔掉电脑的电源线然后静静去世（雾）（不会真有人知道现在要干嘛吧 不会吧）。")
+                        }
+                    } else {
+                        "cannot run fastboot devices ,error=9,$s \n请尝试下载完整包。".run {
+                            throw IOException(this)
+                        }
+                    }
+                    Thread.sleep(1024)
+                }
+            } catch (e: IOException) {
+                TextDialog {
+                    e.toString()
+                }
+                e.printStackTrace()
+            }
+        }
+    }
+
+}
--- me/heizi/swing/swingx.kt
+++ me/heizi/swing/swingx.kt
@@ -17,124 +17,11 @@ import java.io.IOException
 import javax.swing.*
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
-}
-
-fun waitForDeviceFastboot(dosth:()->Unit){
-    var dialog:JDialog? = null
-    GlobalScope.launch {
-        launch(IO){
-            dialog = TextDialog(
-                title = "等待设备",
-                show = true
-            ){
-                "等待设备中，当检测到单个fastboot设备后，软件会自动跳转到下一个步骤。请将手机重启到fastboot再启动本软件，并且检查驱动是否成功安装到你的电脑上。" +
-                        "<br />相关视频：如何给电脑安装fastboot驱动 <br /> <a href='https://www.bilibili.com/video/BV1n64y1u7LE/'>https://www.bilibili.com/video/BV1n64y1u7LE/</a>"
-            }
-        }
-
-        launch(Default) {
-            try {
-                loop@ while (true) {
-                    if (dialog != null){if (!dialog!!.isVisible) {
-                        println("窗口关闭")
-                        break@loop
-                    }}
-                    val (b, s) = platformTool fastboot "devices"
-                    if (b) {
-                        when (s!!.lines().size) {
-                            0 -> throw IOException("未知错误")
-                            1 -> log("未检测到设备")
-                            2 -> {
-                                dialog?.isVisible = false
-                                dosth()
-                                break@loop
-                            }
-                            else -> println("多台设备，请拔掉电脑的电源线然后静静去世（雾）（不会真有人知道现在要干嘛吧 不会吧）。")
-                        }
-                    } else {
-                        "cannot run fastboot devices ,error=9,$s \n请尝试下载完整包。".run {
-                            throw IOException(this)
-                        }
-                    }
-                    Thread.sleep(1024)
-                }
-            } catch (e: IOException) {
-                TextDialog {
-                    e.toString()
-                }
-                e.printStackTrace()
-            }
-        }
-    }
-
-}
 
 
-//suspend fun PlatformTools.Fastboot.waitForDevice( ) :Boolean {
-//    var dialog:JDialog? = null
-//    var rr = false
-//    fun dialogVisible(boolean: Boolean) {
-//        GlobalScope.launch(Dispatchers.IO) { dialog!!.isVisible = boolean }
-//    }
-//     suspend fun getbbbbbb():Boolean =  withContext(Dispatchers.IO){
-//        try {
-//            loop@ while (true) {
-//                if (dialog!=null) if (!dialog!!.isVisible) break@loop
-//                val (b, s) = platformTool fastboot "devices"
-//                if (b) {
-//                    when (s!!.lines().size) {
-//                        0 -> {
-//                            throw IOException("未知错误")
-//                        }
-//                        1 -> {
-//                            println("未检测到设备")
-//                        }
-//                        2 -> {
-//                            dialogVisible(false)
-//                            rr = true
-//                            return@withContext true
-//                        }
-//                        else -> {
-//                            println("多台设备")
-//                        }
-//                    }
-//                } else {
-//                    "cannot run fastboot devices ,error=9,$s \n请尝试下载完整包。".run {
-//
-//                        throw IOException(this)
-//                    }
-//                }
-//                Thread.sleep(1024)
-//            }
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//            dialogVisible(false)
-//        }
-//
-//        false
-//    }
-//
-//     GlobalScope.launch(Dispatchers.IO) {
-//        dialog = Dialog {
-//            title = "等待设备中"
-//            Label {
-//                "等待设备中，当检测到单个fastboot设备后，软件会自动跳转到下一个步骤。请将手机重启到fastboot再启动本软件，并且检查驱动是否成功安装到你的电脑上。" +
-//                        "<br />相关视频：如何给电脑安装fastboot驱动 <br /> <a href='https://www.bilibili.com/video/BV1n64y1u7LE/'>https://www.bilibili.com/video/BV1n64y1u7LE/</a>"
-//            }
-//            isVisible = true
-//        }
-//    }
-//    return getbbbbbb()
-//}
 
 
-open class swingx {
-
-}
-//fun Window.getFile():
 fun Window.getFile(): String? = getFile(this)
-
 fun getFile(parent:Component = frame!! ):String? = JFileChooser().apply {
     isMultiSelectionEnabled = false
     showOpenDialog(parent)
@@ -143,9 +30,8 @@ fun getFile(parent:Component = frame!! ):String? = JFileChooser().apply {
 
 
 
-fun showDialogAsResult(commandResult: CommandResult):Dialog = Dialog {
+fun showDialogAsResult(commandResult: CommandResult,title: String = "执行结果"):Dialog = Dialog {
     val (b,s) = commandResult
-    title = "执行结果"
     Label { "执行"+ if (b){ "成功：" }else{ "失败：" } +"\n $s"}
     setSize(300,300)
 }
@@ -172,25 +58,13 @@ private val dialogDefaultSetting : JDialog.()->Unit = {
     setLocationRelativeTo(null)
 }
 fun Dialog(
-
     frame:Frame = me.heizi.swing.frame!!,
     title:String = dialogDefaultTitle,
     modal :Boolean = false,
     show:Boolean =true,
     apply: JDialog.() -> Unit
 ):JDialog = JDialog(frame,title,modal).apply(apply).apply(dialogDefaultSetting).apply { this.isVisible = show }
-//fun Dialog(frame: Frame,title: String,apply: JDialog.() -> Unit):JDialog = JDialog(frame,title).apply(apply).apply(apply)
-//fun Dialog(frame: Frame,apply: JDialog.() -> Unit):JDialog = JDialog(frame).apply(dialogDefaultSetting).apply(apply)
-//fun Dialog(apply: JDialog.() -> Unit):JDialog =JDialog(frame!!).apply(dialogDefaultSetting).apply(apply)
 
-//class TextDialog(block:TextDialog.()->String){
-//    var frame = me.heizi.swing.frame!!
-//    var title = dialogDefaultTitle
-//    var labelSetting : JLabel.()->Unit = {}
-//    var dialog: Dialog = Dialog (frame = frame,title = title) {
-//        this.Label{block()}.run(labelSetting)
-//    }
-//}
 fun TextDialog(
     frame: Frame =me.heizi.swing.frame!!,
     title: String = dialogDefaultTitle,
@@ -236,15 +110,4 @@ fun JPanel.Button(name:String = "按钮",apply: MouseAdapter.(MouseEvent?) -> Un
 }.also{add(it)}
 
 fun Container.Label(getString: () -> String) = JLabel(getString().toHtml()).also { add(it) }
-
-inline infix fun Boolean.True(block: (Boolean)->Unit):Boolean = this.also { if (it) block(this) }
-inline infix fun Boolean.False(block:(Boolean)->Unit):Boolean = this.also { if (!it) block(this) }
-fun String.toHtml(): String = """<html>
-    <body><div>${this.replace("\n","<br />")}</div>
-</body></html>""".trimIndent().println()
-
-fun <T> T.println() : T {
-    kotlin.io.println(toString())
-    return this
-}
 
